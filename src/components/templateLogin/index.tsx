5c4114c6977bb39a471214e8e6ee2ab3fe6e5c53
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-11-26 00:18:25 -0300

    validation login - frontend

--- src/components/templateLogin/index.tsx
+++ src/components/templateLogin/index.tsx
@@ -1,6 +1,6 @@
-import { motion } from "framer-motion";
+import { motion } from "framer-motion"
 import { useState, ChangeEvent } from 'react'
-import axios from "axios";
+import axios from "axios"
 
 import styles from './styles.module.scss'
 
@@ -15,50 +15,44 @@ export function TemplateLogin() {
 
     const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
         setValorEmail(event.target.value)
-        
-        if (isSubmitted) {
-            setErrorEmail(!event.target.value);
-        }
-    };
+    }
 
     const handlePwChange = (event: ChangeEvent<HTMLInputElement>) => {
         setValorPw(event.target.value)
 
         if (isSubmitted) {
-            setErrorPw(!event.target.value);
-        }
-    };
-
-    const handleSubmit = async () => {
-        setIsSubmitted(true);
-        setErrorEmail(!valueEmail);
-        setErrorPw(!valuePw);
-
-        if (!errorEmail && !errorPw) {
-            try {
-                const response = await axios.post('http://localhost:4000/login', {
-                    email: valueEmail,
-                    pw: valuePw,
-                });
-         
-                if (response.status === 200) {
-                    console.log('Comeu todas')
-                } else {
-                    console.log('Se fodeu')
-                }
-            } catch (error) {
-                console.error('Erro ao enviar dados para o backend:', error)
-            }
+            setErrorPw(!event.target.value)
         }
     }
 
-    const handleCreateAccount = () => { window.location.href =  'http://localhost:5173/createAccount'}
+    const handleSubmit = async () => {
+        setIsSubmitted(true)
+        try {
+            const response = await axios.post('http://localhost:4000/login', {
+                email: valueEmail,
+                pw: valuePw,
+            })
+
+            if (response.status === 200) {
+                window.location.href = '/account'
+                console.log('Login successful!')
+            } else {
+                console.log('Login unsuccessful')
+            }
+        } catch (error) {
+            setErrorEmail(true)
+            setErrorPw(true)
+            console.error('Erro ao enviar dados para o backend')
+        }
+    }
+
+    const handleCreateAccount = () => { window.location.href = 'http://localhost:5173/createAccount' }
 
     return (
         <div className={styles.exampleContainer}>
-            <motion.div 
-                className={styles.templateLogin} 
-                whileHover={{ scale: 1.04 }} whileTap={{ scale: 0.95 }} 
+            <motion.div
+                className={styles.templateLogin}
+                whileHover={{ scale: 1.04 }} whileTap={{ scale: 0.95 }}
                 initial={{ y: -500, rotateY: 100 }}
                 animate={{ y: 0, rotateY: 0, transition: { duration: 0.7 } }}
             >

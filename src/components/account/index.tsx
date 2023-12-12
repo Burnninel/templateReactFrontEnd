28b849bb5929e8b15682f1cbddd9f05e5de41759
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-12-11 23:53:14 -0300

    Creating the User Profile

--- src/components/account/index.tsx
+++ src/components/account/index.tsx
@@ -2,18 +2,39 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import myPhoto from './my photo.jpg';
+
 import styles from './styles.module.scss';
+import { IconEdit, IconConfirm, IconSearch, IconMap } from "../icons/icons";
 
 type User = {
   id: string
   email: string;
   name: string;
   profession: string,
+  phone: string,
+}
+
+type Address = {
+  logradouro: string
+  bairro: string;
+  localidade: string;
+  uf: string,
+  phone: string,
 }
 
 export function TemplateAccount() {
 
   const [userData, setUserData] = useState<User | null>(null)
+
+  const [email, setEmail] = useState('')
+  const [emailState, setEmailState] = useState(true)
+
+  const [phone, setPhone] = useState('')
+  const [phoneState, setPhoneState] = useState(true)
+
+  const [cep, setCep] = useState('');
+  const [address, setAddress] = useState<Address | null>(null)
 
   useEffect(() => {
 
@@ -28,6 +49,8 @@ export function TemplateAccount() {
     axios.get('http://localhost:4000/account', config)
       .then(response => {
         setUserData(response.data)
+        setEmail(response.data.email);
+        setPhone(response.data.phone)
       })
       .catch(error => {
         console.error(error);
@@ -35,19 +58,103 @@ export function TemplateAccount() {
 
   }, []);
 
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleEmailState = () => {
+    setEmailState(false);
+  };
+
+  const handleEmailConfirm = () => {
+    setEmailState(true);
+  };
+
+  const handlePhoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(event.target.value);
+  };
+
+  const handlePhoneState = () => {
+    setPhoneState(false);
+  };
+
+  const handlePhoneConfirm = () => {
+    setPhoneState(true);
+  };
+
+  const handleCepChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCep(event.target.value);
+  };
+
+  const fetchCep = async () => {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      setAddress(response.data);
+    } catch (error) {
+      console.error('Erro ao buscar CEP:', error);
+    }
+
+  };
+
   return (
-    <div className={styles.exampleContainer}>
-      <motion.div
-        className={styles.templateAccount}
-        whileHover={{ scale: 1.04 }} whileTap={{ scale: 1 }}
-        initial={{ y: -500, rotateY: 100 }}
-        animate={{ y: 0, rotateY: 0, transition: { duration: 0.7 } }}
-      >
-        <h3 id={styles.titleCreateAccount}>{userData ? userData.id : 'Loading...'}</h3>
-        <h3 id={styles.titleCreateAccount}>{userData ? userData.name : 'Loading...'}</h3>
-        <h3 id={styles.titleCreateAccount}>{userData ? userData.profession : 'Loading...'}</h3>
-        <h3 id={styles.titleCreateAccount}>{userData ? userData.email : 'Loading...'}</h3>
-      </motion.div>
-    </div>
+    <>
+      <div className={styles.template}>
+        <motion.div
+          className={styles.templateAccount}
+          whileHover={{ scale: 1.04 }} whileTap={{ scale: 0.99999999999 }}
+          initial={{ y: -500, rotateY: 100 }}
+          animate={{ y: 0, rotateY: 0, transition: { duration: 0.7 } }}
+        >
+
+          <div className={styles.leftProfile}>
+            <div className={styles.photoProfile}>
+              <img src={myPhoto} alt="myphoto" />
+            </div>
+          </div>
+
+          <div className={styles.rightProfile}>
+            <div className={styles.presentation}>
+              <h3 className={styles.userName}>{userData ? userData.name : 'Loading...'}</h3>
+              <h3 className={styles.userProfession}>{userData ? userData.profession : 'Loading...'}</h3>
+            </div>
+
+            <div className={styles.contact}>
+              <div className={styles.inputGroupContact}>
+                <label className={styles.userEmail}>Email: </label>
+                <input className={styles.textInput} type="text" value={email} disabled={emailState} onChange={handleEmailChange} />
+                <div className={styles.editInfo}>
+                  {emailState ? <IconEdit onClick={handleEmailState} /> : <IconConfirm onClick={handleEmailConfirm} />}
+                </div>
+              </div>
+
+              <div className={styles.inputGroupContact}>
+                <label className={styles.userEmail}>Telefone: </label>
+                <input className={styles.textInput} type="text" value={phone ? phone : 'Não cadastrado...'} disabled={phoneState} onChange={handlePhoneChange} />
+                <div className={styles.editInfo}>
+                  {phoneState ? <IconEdit onClick={handlePhoneState} /> : <IconConfirm onClick={handlePhoneConfirm} />}
+                </div>
+              </div>
+
+              <div className={styles.inputGroupContact}>
+                <label className={styles.userEmail}>CEP: </label>
+                <input className={styles.textInput} type="text" placeholder="Digite seu cep..." maxLength={8} onChange={handleCepChange} />
+                <div className={styles.editInfo}>
+                  <IconSearch onClick={fetchCep} />
+                </div>
+              </div>
+
+              <div className={styles.address}> 
+                {address && <IconMap />}
+                <span className={styles.textAddress}>
+                  {address ? address.logradouro + ', ' + address.localidade + '/' + address.uf + '.' : ''}
+                </span>
+              </div>
+
+            </div>
+          </div>
+
+        </motion.div>
+      </div>
+    </>
   );
 }
---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,26 @@
+import styles from '../../components/account/styles.module.scss'
+
+export const IconEdit = ({ onClick }: { onClick?: () => void }) => (
+    <svg className={styles.icon} onClick={onClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+    </svg>
+);
+
+export const IconConfirm = ({ onClick }: { onClick?: () => void }) => (
+    <svg className={styles.icon} onClick={onClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+    </svg>
+);
+
+export const IconSearch = ({ onClick }: { onClick?: () => void }) => (
+    <svg className={styles.icon} onClick={onClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+    </svg>
+);
+
+export const IconMap = () => (
+    <svg className={styles.IconMap} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+    </svg>
+);
